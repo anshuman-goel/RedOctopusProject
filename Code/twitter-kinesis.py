9a71a8eb9b231f88d68566661ff131d8c83ceedd
agoel5 <agoel5@ncsu.edu>
2017-10-08 12:53:33 -0400

    Streaming data from twiter and putting it into kinesis

--- Code/twitter-kinesis.py
+++ Code/twitter-kinesis.py
@@ -1,4 +1,4 @@
-# Example to use twitter api and feed data into kinesis
+## Example to use twitter api and feed data into kinesis
 
 from TwitterAPI import TwitterAPI
 import boto3
@@ -8,7 +8,6 @@ import twitterCreds
 ## twitter credentials
 
 consumer_key = twitterCreds.consumer_key
-print(consumer_key)
 consumer_secret = twitterCreds.consumer_secret
 access_token_key = twitterCreds.access_token_key
 access_token_secret = twitterCreds.access_token_secret
@@ -17,6 +16,15 @@ api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_s
 
 kinesis = boto3.client('kinesis')
 
-request = api.request('statuses/filter', {'locations':'-90,-90,90,90'})
-for item in request:
-        kinesis.put_record(StreamName="twitter", Data=json.dumps(item), PartitionKey="filler")
+while (1):
+    r = api.request('statuses/user_timeline', {'count':200})
+    tweets = []
+    count = 0
+    for item in r:
+            jsonItem = json.dumps(item)
+            tweets.append({'Data':jsonItem, 'PartitionKey':"filler"})
+            count += 1
+            if count == 100:
+                    kinesis.put_records(StreamName="twitter", Records=tweets)
+                    count = 0
+                    tweets = []
--- Code/twitterCreds.py
+++ Code/twitterCreds.py
@@ -4,5 +4,5 @@
 ## obtain/create here: https://apps.twitter.com/
 consumer_key = "91jSFcd0Ae9Jewcl65ZkN5ZxE"
 consumer_secret = "WkVNfI4ritCBTIzf8qAZULmwUANSMUuxcaqU7ITw69QZDHaYwE"
-access_token_key = "53382302-SRfR9Xfmiq7bahVNHgkYJFHN4xIUQLAZJ1lVE6iu"
+access_token_key = "53382302-2SRfR9Xfmiq7bahVNHgkYJFHN4xIUQLAZJ1lVE6iu"
 access_token_secret = "UvMPkFFytlkY6gdTHVANbhpafgQ15mgblnQese6wRxgt6"
