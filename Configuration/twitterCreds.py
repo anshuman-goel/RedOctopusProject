20eba8c57dddbf13ea76cc8c7acfae65f8bc69de
agoel5 <agoel5@ncsu.edu>
2017-10-08 11:51:17 -0400

    Requirement and twitter consumer key

---
+++ Configuration/twitterCreds.py
@@ -0,0 +1,8 @@
+## template of what twitterCreds.py should look like
+## the actual twitterCreds file should contain
+## your actual Twitter credentials that one can
+## obtain/create here: https://apps.twitter.com/
+consumer_key = "91jSFcd0Ae9Jewcl65ZkN5ZxE"
+consumer_secret = "WkVNfI4ritCBTIzf8qAZULmwUANSMUuxcaqU7ITw69QZDHaYwE"
+access_token_key = "53382302-SRfR9Xfmiq7bahVNHgkYJFHN4xIUQLAZJ1lVE6iu"
+access_token_secret = "UvMPkFFytlkY6gdTHVANbhpafgQ15mgblnQese6wRxgt6"
