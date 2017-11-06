9024fa6eb7fb030fdbc39293c94ea565b203e39c
agoel5 <agoel5@ncsu.edu>
2017-11-05 21:36:25 -0500

    lobal variable added to communicate with various files

--- Code/create-stream.py
+++ Code/create-stream.py
@@ -1,4 +1,5 @@
 import boto3
+from global_variables import *
 
 client = boto3.client('kinesis')
-response = client.create_stream(StreamName='twitter', ShardCount=5)
+response = client.create_stream(StreamName='twitter', ShardCount=shard_count)
