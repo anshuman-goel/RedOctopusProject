628a9a5d9888574a8d54fbb8817649655ec99283
vvnadimp <vvnadimp@ncsu.edu>
2017-11-09 10:49:06 -0500

    twitter credentials added

--- Code/twitterCreds.py
+++ Code/twitterCreds.py
@@ -2,11 +2,25 @@
 ## the actual twitterCreds file should contain
 ## your actual Twitter credentials that one can
 ## obtain/create here: https://apps.twitter.com/
-#consumer_key = "91jSFcd0Ae9Jewcl65ZkN5ZxE"
-#consumer_secret = "WkVNfI4ritCBTIzf8qAZULmwUANSMUuxcaqU7ITw69QZDHaYwE"
-#access_token_key = "53382302-2SRfR9Xfmiq7bahVNHgkYJFHN4xIUQLAZJ1lVE6iu"
-#access_token_secret = "UvMPkFFytlkY6gdTHVANbhpafgQ15mgblnQese6wRxgt6"
+
+#agoel5
+'''
 consumer_key = "91jSFcd0Ae9Jewcl65ZkN5ZxE"
 consumer_secret = "WkVNfI4ritCBTIzf8qAZULmwUANSMUuxcaqU7ITw69QZDHaYwE"
 access_token_key = "53382302-2SRfR9Xfmiq7bahVNHgkYJFHN4xIUQLAZJ1lVE6iu"
 access_token_secret = "UvMPkFFytlkY6gdTHVANbhpafgQ15mgblnQese6wRxgt6"
+'''
+
+# vvnadimp
+consumer_key = "qTUnsFS9CrLAqyY0VPQcXHV78"
+consumer_secret = "YQbUtiI3fF3SbkeNX8yjEtkvHg7BEz1JdrTPO7GZkhLAiptHMB"
+access_token_key = "811951139843375104-7lsugkdTI4IPwLWff8GtzvAcvIoIIvS"
+access_token_secret = "8b7VEndwlfg4gHVcucBGo1VjjSb3AgTeScYDEN576LsJy"
+
+# rthiran
+'''
+consumer_key = "CzsaYYCSBw1GACyGlgcHwuPk"
+consumer_secret = "TD1o8eovRJsSaafgmEdn4Ag7AJAcJHQS6uieV6nLeVCAODlqfN"
+access_token_key = "2352880537-rONxPfB1s6jZvrxd9QPVnNLDE36V6xMPZ74DH5c"
+access_token_secret = "npNZMz3eJin3tnDGmgDxFXfykHVfjjkYzBbINVgCHQz8J"
+'''
