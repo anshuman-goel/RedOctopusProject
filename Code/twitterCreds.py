c7128c800b0b34b61fd47e76e3ea13f895374522
rachit thirani <rthiran@ncsu.edu>
2017-11-09 10:42:15 -0500

    twitter cred

--- Code/twitterCreds.py
+++ Code/twitterCreds.py
@@ -2,6 +2,10 @@
 ## the actual twitterCreds file should contain
 ## your actual Twitter credentials that one can
 ## obtain/create here: https://apps.twitter.com/
+#consumer_key = "91jSFcd0Ae9Jewcl65ZkN5ZxE"
+#consumer_secret = "WkVNfI4ritCBTIzf8qAZULmwUANSMUuxcaqU7ITw69QZDHaYwE"
+#access_token_key = "53382302-2SRfR9Xfmiq7bahVNHgkYJFHN4xIUQLAZJ1lVE6iu"
+#access_token_secret = "UvMPkFFytlkY6gdTHVANbhpafgQ15mgblnQese6wRxgt6"
 consumer_key = "91jSFcd0Ae9Jewcl65ZkN5ZxE"
 consumer_secret = "WkVNfI4ritCBTIzf8qAZULmwUANSMUuxcaqU7ITw69QZDHaYwE"
 access_token_key = "53382302-2SRfR9Xfmiq7bahVNHgkYJFHN4xIUQLAZJ1lVE6iu"
