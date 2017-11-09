f3f5be106b1f6730f9a40a2fb31f6fe6f0feacda
rachit thirani <rthiran@ncsu.edu>
2017-11-09 12:15:01 -0500

    twitter cred for rthiran updated

--- Code/twitterCreds.py
+++ Code/twitterCreds.py
@@ -12,15 +12,15 @@ access_token_secret = "UvMPkFFytlkY6gdTHVANbhpafgQ15mgblnQese6wRxgt6"
 '''
 
 # vvnadimp
+'''
 consumer_key = "qTUnsFS9CrLAqyY0VPQcXHV78"
 consumer_secret = "YQbUtiI3fF3SbkeNX8yjEtkvHg7BEz1JdrTPO7GZkhLAiptHMB"
 access_token_key = "811951139843375104-7lsugkdTI4IPwLWff8GtzvAcvIoIIvS"
 access_token_secret = "8b7VEndwlfg4gHVcucBGo1VjjSb3AgTeScYDEN576LsJy"
+'''
 
 # rthiran
-'''
-consumer_key = "CzsaYYCSBw1GACyGlgcHwuPk"
+consumer_key = "CCzsaYYCSBw1GACyGlgcHwuPk"
 consumer_secret = "TD1o8eovRJsSaafgmEdn4Ag7AJAcJHQS6uieV6nLeVCAODlqfN"
 access_token_key = "2352880537-rONxPfB1s6jZvrxd9QPVnNLDE36V6xMPZ74DH5c"
 access_token_secret = "npNZMz3eJin3tnDGmgDxFXfykHVfjjkYzBbINVgCHQz8J"
-'''
