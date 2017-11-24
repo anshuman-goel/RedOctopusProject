89346df7f860130f86dd4bfbb2872c4bf693c5f8
Anshuman Goel <agoel5@ncsu.edu>
2017-11-23 19:53:18 -0500

    merge resolved

--- Code/twitterCreds.py
+++ Code/twitterCreds.py
@@ -4,12 +4,10 @@
 ## obtain/create here: https://apps.twitter.com/
 
 #agoel5 - 1
-'''
 consumer_key = "91jSFcd0Ae9Jewcl65ZkN5ZxE"
 consumer_secret = "WkVNfI4ritCBTIzf8qAZULmwUANSMUuxcaqU7ITw69QZDHaYwE"
 access_token_key = "53382302-2SRfR9Xfmiq7bahVNHgkYJFHN4xIUQLAZJ1lVE6iu"
 access_token_secret = "UvMPkFFytlkY6gdTHVANbhpafgQ15mgblnQese6wRxgt6"
-'''
 
 #agoel5 - 2
 '''
@@ -31,16 +29,20 @@ access_token_secret = "8b7VEndwlfg4gHVcucBGo1VjjSb3AgTeScYDEN576LsJy"
 consumer_key = "YLSNm8CAPsGOxkh9cBqoYZuxX"
 consumer_secret = "BKpJtLC5i4bnlfAq5aiNoNPImmYgIHLRjxfNSTPaaTHVf5jGZW"
 access_token_key = "811951139843375104-OzdV3TVMWaR7sEZpXp1BGWXjmx7G1z4"
-access_token_secret = "jWDkBXcHBvvfAnxFeT5JBAKZnJXICqkrwzrwiajg3FBfS"'''
+access_token_secret = "jWDkBXcHBvvfAnxFeT5JBAKZnJXICqkrwzrwiajg3FBfS"
 '''
 # rthiran - 1
+'''
 consumer_key = "CCzsaYYCSBw1GACyGlgcHwuPk"
 consumer_secret = "TD1o8eovRJsSaafgmEdn4Ag7AJAcJHQS6uieV6nLeVCAODlqfN"
 access_token_key = "2352880537-rONxPfB1s6jZvrxd9QPVnNLDE36V6xMPZ74DH5c"
 access_token_secret = "npNZMz3eJin3tnDGmgDxFXfykHVfjjkYzBbINVgCHQz8J"
 '''
+
 # rthiran - 2
+'''
 consumer_key = "CfRzkbk1fBgnTO3GTYRiwqZE1"
 consumer_secret = "3YICSbi1JTjisbuhJQjgPKvqqJNGO9arGvw5DiSD2ifgBwBKVV"
 access_token_key = "2352880537-Q3ku7VUt4UbSS5UEFo17d1NngZDl09T5JFSQJzc"
 access_token_secret = "hefa29qhibPQvRyoD62lfRUZEpEKQmoaC0okIY6qlt76O"
+'''
